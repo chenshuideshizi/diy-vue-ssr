450ab9a32c85b345403503661baaa2ed78177221
fanwenxiang <fanwenxiang@aiforward.com>
2021-10-20 13:25:28 +0800

    feature:express服务正常启动

--- serve.express.js
+++ serve.express.js
@@ -1,24 +1,26 @@
-const Vue = require('vue')
-const server = require('express')()
+const express = require('express')
 const { createBundleRenderer } = require('vue-server-renderer')
 const fs = require('fs')
+
+const app = express()
 
 const template = fs.readFileSync("./public/index.ssr.html", "utf8")
 const clientManifest = require("./dist/vue-ssr-client-manifest.json")
 const serverBundle = require("./dist/vue-ssr-server-bundle.json")
+
 const renderer = createBundleRenderer(serverBundle, {
   runInNewContext: false, // 推荐
   template, // （可选）页面模板
   clientManifest // （可选）客户端构建 manifest
 })
 
+app.use('/auto', express.static('./dist'))
 
-server.get('*', (req, res) => {
+app.get('*', (req, res) => {
   const context = { url: req.url }
 
   res.setHeader('Content-type','text/html;charset=utf-8')
   renderer.renderToString(context, (err, html) => {
-    // 处理异常……
     if (err) {
       res.status(500).end('Internal Server Error')
       return
@@ -27,6 +29,6 @@ server.get('*', (req, res) => {
   })
 })
 
-server.listen(8080, () => {
+app.listen(8080, () => {
   console.log(`server is listen on http://localhost:8080`)
 })
---
+++ serve.koa.js
@@ -0,0 +1,44 @@
+const Koa = require('koa')
+const fs = require('fs')
+const KoaStatic = require('koa-static')
+
+const app = new Koa()
+
+const { createBundleRenderer } = require('vue-server-renderer')
+
+const template = fs.readFileSync("./public/index.ssr.html", "utf8")
+const clientManifest = require("./dist/vue-ssr-client-manifest.json")
+const serverBundle = require("./dist/vue-ssr-server-bundle.json")
+
+const renderer = createBundleRenderer(serverBundle, {
+  runInNewContext: false, // 推荐
+  template, // （可选）页面模板
+  clientManifest // （可选）客户端构建 manifest
+})
+
+app.use(KoaStatic('./dist'))
+
+app.use(async (ctx, next) => {
+  const context = { url: ctx.url }
+
+  try {
+    // 根据请求路径去返回给server-entry url路径
+    ctx.body = await new Promise((resolve, reject) => {
+      renderer.renderToString(context, (err, html) => {
+        if (err) {
+          resolve('Internal Server Error')
+          return
+        }
+        resolve(html)
+      })
+    })
+  } catch (err) {
+    ctx.body = err.code
+  }
+
+  await next()
+})
+
+app.listen(3000, () => {
+  console.log(`node serve run at port http://localhost:3000`)
+})
